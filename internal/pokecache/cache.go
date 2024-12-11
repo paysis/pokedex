aeba85ed88bc8a62d221e2546c0de60a3864f6b1
paysis <paysis21@gmail.com>
2024-12-11 03:04:14 +0300

    add cache package and respective unit tests

---
+++ internal/pokecache/cache.go
@@ -0,0 +1,56 @@
+package pokecache
+
+import (
+	"bytes"
+	"sync"
+	"time"
+)
+
+type Cache struct {
+	entries map[string]cacheEntry
+	lock    *sync.Mutex
+}
+
+type cacheEntry struct {
+	createdAt time.Time
+	val       *bytes.Buffer
+}
+
+func NewCache(interval time.Duration) Cache {
+	cache := Cache{
+		entries: map[string]cacheEntry{},
+		lock:    &sync.Mutex{},
+	}
+	go cache.reapLoop(interval)
+	return cache
+}
+
+func (c Cache) Add(key string, val *bytes.Buffer) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	c.entries[key] = cacheEntry{
+		createdAt: time.Now(), // as the app is local, lets use local version of time
+		val:       val,
+	}
+}
+
+func (c Cache) Get(key string) (*bytes.Buffer, bool) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	entry, ok := c.entries[key]
+	return entry.val, ok
+}
+
+func (c Cache) reapLoop(interval time.Duration) {
+	ticker := time.NewTicker(interval)
+	defer ticker.Stop()
+	for currentTime := range ticker.C {
+		c.lock.Lock()
+		for k, v := range c.entries {
+			if currentTime.Sub(v.createdAt) > interval {
+				delete(c.entries, k)
+			}
+		}
+		c.lock.Unlock()
+	}
+}
